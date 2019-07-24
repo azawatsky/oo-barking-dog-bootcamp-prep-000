8329a767f6a7e7e3151d53a1a0a230f0f991bbe8
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-07-24 17:02:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,13 @@
 class Dog
-  def initialize(name, bark)
-    @name = name
-    @bark = bark
-  end
- 
   def name=(name)
     @name = name
   end
- 
-  def bark=(bark)
-    @bark = bark
-  end
- 
+
   def name
     @name
   end
 
   def bark
-    puts @bark
+    puts "woof!"
   end
 end
